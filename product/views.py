59cb2eda1f31bfcf97e368fdf479f2c0f6db54c1
ScnuWang <scnu_wang@163.com>
2018-08-03 18:30:50 +0800

    提取产品历史价格列表

--- product/views.py
+++ product/views.py
@@ -9,12 +9,18 @@ def get_product_comment_id(request,original_id):
     return JsonResponse(context)
 
 def get_product_by_id(request,original_id):
-    product = ProductModel.objects.filter(original_id=original_id).order_by('-last_updated').first()
+    # 获取最新的产品信息
+    product = ProductModel.objects.filter(original_id=original_id).first()
+    # 获取产品的历史价格列表
+    product_prict_list = ProductModel.objects.filter(original_id=original_id).order_by('-last_updated').values_list('project_price')
+    product_prict_list = [float(n) for n in product_prict_list]
+    # 获取对应的最新评论的前3条
     comment = CommentModel.objects.filter(project_id=int(original_id)).order_by('-last_updated').limit(3)
     # print('=====================>',product)
     context = {}
     context['product'] = product
     context['comments'] = comment
+    context['product_prict_list'] = product_prict_list
     return render(request,'product/product_detail.html',context)
 
 
