1c92b4776f55387bf63cde218032a1430cbdfcad
ScnuWang <scnu_wang@163.com>
2018-08-03 17:13:30 +0800

    完善评论展示以及布局

--- product/urls.py
+++ product/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.get_product_list, name='product_list'),
     path('<str:original_id>', views.get_product_by_id, name='product_detail'),
+    path('<str:original_id>', views.get_product_comment_id, name='product_comment'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,20 +1,35 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import ProductModel,CommentModel
 
+def get_product_comment_id(request,original_id):
+    comment = CommentModel.objects.filter(project_id=int(original_id)).order_by('-last_updated').limit(3)
+    context = {}
+    context['comments'] = comment
+    return JsonResponse(context)
 
 def get_product_by_id(request,original_id):
-    print('=====================>',original_id)
-    product = ProductModel.objects.first()
-    comment = CommentModel.objects.first()
+    product = ProductModel.objects.filter(original_id=original_id).order_by('-last_updated').first()
+    comment = CommentModel.objects.filter(project_id=int(original_id)).order_by('-last_updated').limit(3)
+    # print('=====================>',product)
     context = {}
     context['product'] = product
-    context['comment'] = comment
+    context['comments'] = comment
     return render(request,'product/product_detail.html',context)
 
 
 def get_product_list(request):
-    product_list = ProductModel.objects.filter(original_id='1478002')
+
+    # 关键字查询
+    if request.POST.get('keyword'):
+        keyword = request.POST.get('keyword')
+        product_list = ProductModel.objects(project_name__contains=keyword)
+    else:
+        product_list = ProductModel.objects.limit(10)
 
     context = {}
     context['product_list'] = product_list
     return render(request,'product/product_list.html',context)
+
+def keyword_serach(request):
+    pass
